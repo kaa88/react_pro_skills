0273a511740d90f7d06c3a9e1aeadd4101ca5e0b
Andrei <dre021088@gmail.com>
2023-09-01 15:02:03 +0700

    edit gitignore

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Card, { Width } from './components/Card/Card';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	let name = '10'
+
+	return (
+		<div className="App">
+			<Card width={Width.one} />
+			<Card className={name} name='vasya'></Card>
+		</div>
+	)
 }
 
 export default App;
