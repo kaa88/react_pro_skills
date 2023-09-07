5e7e1d550291e0fe8ca549ca14dbccfa8e61138f
Andrei <dre021088@gmail.com>
2023-09-07 14:05:57 +0700

    migrate to TS

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,12 @@
-import React from 'react';
-// import './App.css';
-// import * as CardComponent from './components/Card/Card';
-
-// console.log(Card.Width);
-// const Card = CardComponent.default
+import './styles/index.scss';
+import Router from './router/Router';
 
 
 function App() {
-	let name = '10'
 
 	return (
-		<div className="App">
-			{/* <Card width={CardComponent.Width.one} />
-			<Card className={name} name='vasya'></Card> */}
-		</div>
+		<Router />
 	)
 }
 
-export default App;
+export default App
